91f49b4cb3c4982dce1f91c51e04d7e2916b6545
Thai Nguyen <59272463+ThaiNguyen54@users.noreply.github.com>
2022-10-28 08:22:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import Router from "./routes/routes.js";
 import Mongoose from "mongoose";
 import MongoConfig from './config/MongoDBConfig.js'
-import {AllowCrossDomain} from "./middlewares/allowCrossDomain.js";
 // 2 lines below connect to MongoDB
 Mongoose.Promise = global.Promise;
 Mongoose.connect(MongoConfig.mongodb.uri)
@@ -11,10 +10,9 @@ Mongoose.connect(MongoConfig.mongodb.uri)
 
 const app = express();
 app.use(express.json());
-//app.use(cors());
-app.all('/*', AllowCrossDomain);
+app.use(cors());
 app.use(Router);
 
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 5000;
 app.listen(port, () => console.log('Server running at http://localhost:3000'));
 
